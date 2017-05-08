200184a010bc83d2cab18465c4e454746c55a0cf
Travis <tlgreg86@gmail.com>
2017-05-08 04:22:06 -0600

    Got selected cards to render and compare. Still needs some styling and bugs fixed

--- src/Controls.js
+++ src/Controls.js
@@ -1,19 +1,23 @@
 import React from 'react';
 import PropTypes from 'prop-types'
 
-export const Controls = ({onChange}) => {
+export const Controls = ({ onChange, resetComparison }) => {
 
     return(
       <section className='controls-container'>
         <input className='input-field'
                 type='text'
                 placeholder='Search by district'
-                onChange={(event) => onChange(event.target.value)}
-          />
+                onChange={(event) => onChange(event.target.value)} />
+        <input  className="clear-button"
+                type='submit'
+                value='CLEAR COMPARISON'
+                onClick={ () => resetComparison() } />
       </section>
     )
 }
 
 Controls.propTypes = {
   onChange: PropTypes.func.isRequired,
+  resetComparison: PropTypes.func.isRequired
 }
